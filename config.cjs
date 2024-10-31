e2c159e8377017f0636b3cec95a011501e679949
SK HIRON <130884770+ILUCIZR@users.noreply.github.com>
2024-10-31 12:27:15 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -21,8 +21,8 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "✪⏤͟͞★⃝ꪶ‎𝞢𝙏𝞖𝞘𝞦-𝞛𝘿𖥘✪͜͡➺",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "919142294671",
+  OWNER_NAME: process.env.OWNER_NAME || "⎯͢⎯⃝.🪽𝙇𝙪𝙘𝙞 ۵⋆⃝❤‍🩹⋆⃝𝄞",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "918293838182",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
